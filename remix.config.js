3482698ea958297d2c831f6de8bd0065949c3c8e
Per Søderlind <per@soderlind.no>
2022-09-14 00:21:56 +0200

    Back to basics

--- remix.config.js
+++ remix.config.js
@@ -1,31 +1,8 @@
-/**
- * @type {import('@remix-run/dev').AppConfig}
- */
+/** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
-  cacheDirectory: "./node_modules/.cache/remix",
-  ignoredRouteFiles: ["**/.*", "**/*.css", "**/*.test.{js,jsx,ts,tsx}"],
-  serverDependenciesToBundle: [
-    /^rehype.*/,
-    /^remark.*/,
-    /^unified.*/,
-    "marked",
-    "mdx-bundler",
-    "micromark-extension-gfm",
-    "hast-util-has-property",
-    "mdast-util-gfm",
-    "lowlight",
-    "hast-util-heading-rank",
-    "hast-util-to-text",
-    "hast-util-to-string",
-    "unist-util-visit",
-    "hast-util-is-element",
-  ],
-  mdx: async (filename) => {
-    const [rehypeHighlight] = await Promise.all([
-      import("rehype-highlight").then((mod) => mod.default),
-    ]);
-    return {
-      rehypePlugins: [rehypeHighlight],
-    };
-  },
+  ignoredRouteFiles: ["**/.*"],
+  // appDirectory: "app",
+  // assetsBuildDirectory: "public/build",
+  // serverBuildPath: "build/index.js",
+  // publicPath: "/build/",
 };
