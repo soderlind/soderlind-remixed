45332946b00bda8d20ab69ccaaa0a56b4f2d3a6e
Per Søderlind <per@soderlind.no>
2022-09-14 00:32:40 +0200

    Get all jekyll files and list them

--- app/routes/archive/index.tsx
+++ app/routes/archive/index.tsx
@@ -1,76 +1,69 @@
-import styles from "highlight.js/styles/night-owl.css";
-import { Outlet } from "@remix-run/react";
+// import styles from "highlight.js/styles/night-owl.css";
+// import { Outlet } from "@remix-run/react";
 import type { LoaderFunction } from "@remix-run/node"; // or cloudflare/deno
 import { redirect, json } from "@remix-run/node"; // or cloudflare/deno
 
 import { Link, useLoaderData } from "@remix-run/react";
 
-import { getPosts } from "~/utils/mdx";
-import type { Mdx } from "~/utils/mdx";
+import { getArchive } from "~/utils/archive";
+import type { Mdx } from "~/utils/archive";
+import { IntlDate } from "~/components/IntlDate";
+import { parseJSON } from "date-fns";
 
-export const loader: LoaderFunction = async () => {
-  // return json<{ entries: Mdx[] }>({ entries: await getPosts() });
-  return getPosts() as Promise<Mdx[]>;
+type LoaderData = {
+  entries: Awaited<ReturnType<typeof getArchive>>;
 };
-export const links = () => {
-  return [
-    {
-      rel: "stylesheet",
-      href: styles,
-    },
-  ];
+
+export const loader = async () => {
+  return json<LoaderData>({ entries: await getArchive() });
 };
-export default function Posts() {
-  const posts = useLoaderData<Mdx[]>();
+
+export default function Index() {
+  const data = useLoaderData() as LoaderData;
+  const entries = data.entries; // as Post[];
+  const years = Object.keys(entries) as string[];
+  const sortedYears = years.sort((a, b) => b.localeCompare(a));
+
+  const postsByYear = sortedYears.map((year) => {
+    const y = parseInt(year, 10);
+    const posts = entries[y] as unknown as any[];
+    const linksByYear = posts.map((post: Post) => (
+      <li key={post.slug} className="post-preview" id={"post-" + post.slug}>
+        {/* <Link to={"/archive/" + post.slug} className=""> */}
+        <Link to={post.slug} className="">
+          <h2 className="title">
+            <span>{post.title}</span>
+          </h2>
+
+          <IntlDate date={parseJSON(post.date)} timeZone="CET" />
+        </Link>
+      </li>
+    ));
+    return (
+      <li key={year}>
+        <h3 className="list-title">
+          <Link to="#" className="">
+            {year}
+          </Link>
+        </h3>
+        <ul>{linksByYear}</ul>
+      </li>
+    );
+  });
+
+  const sticky = '<div className="sticky-arrow"></div>';
   return (
-    <div>
-      <h1>Posts</h1>
-      <ul>
-        {posts.map((post) => (
-          <li key={post.slug}>
-            <Link to={post.slug}>
-              {post.date}
-              {}
-              {post.title}
-            </Link>
-          </li>
-        ))}
-      </ul>
+    <div className="section-inner">
+      <header className="page-header">
+        <h4 className="page-subtitle">Archive</h4>
+        <div className="page-description">
+          These pages are imported from my jekyll blog. Note, a lot of the
+          content is outdated.
+        </div>
+      </header>
+      <div className="posts" id="posts">
+        <ul>{postsByYear}</ul>
+      </div>
     </div>
   );
 }
-
-// export const loader: LoaderFunction = async ({ request }) => {
-//   const url = new URL(request.url);
-//   const path = url.pathname;
-//   // import Component, { attributes, filename } from `"~${url.pathname}.md"`;
-//   // const content = require(`"${url.pathname}"`);
-//   // console.log(url.pathname);
-//   // console.log(content);
-//   return null;
-// };
-
-// export const links = () => {
-//   return [
-//     {
-//       rel: "stylesheet",
-//       href: styles,
-//     },
-//   ];
-// };
-
-// export default function Old() {
-//   return (
-//     <div className="section-inner">
-//       <header className="page-header">
-//         <h4 className="page-subtitle">Articles</h4>
-//         {/* <div className="page-description"></div> */}
-//       </header>
-//       <div className="posts" id="posts">
-//         <article className="post">
-//           <Outlet />
-//         </article>
-//       </div>
-//     </div>
-//   );
-// }
