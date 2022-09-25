723781663763a82eaceddbab39d52bf4c88a77eb
Per Søderlind <per@soderlind.no>
2022-09-25 23:20:25 +0200

    Format page data

--- app/routes/posts/$slug.tsx
+++ app/routes/posts/$slug.tsx
@@ -19,10 +19,9 @@ export const links: LinksFunction = () => {
 export const loader: LoaderFunction = async ({ params }) => {
   const { slug } = params;
   const page = await client.fetch(
-    groq`*[_type == "page" && slug.current == $slug][0]{ title, content }`,
+    groq`*[_type == "page" && slug.current == $slug][0]{ title, ingress, content, _updatedAt }`,
     { slug }
   );
-
   if (!page) {
     return new Response("Not found", { status: 404 });
   }
@@ -32,13 +31,20 @@ export const loader: LoaderFunction = async ({ params }) => {
 
 export default function Product() {
   const { page } = useLoaderData<{ page: Page }>();
+  console.log(page);
 
   return (
-    <Layout>
-      {page?.title ? <h2 className="entry-title">{page.title}</h2> : null}
-      {page?.content && page.content?.length > 0 ? (
-        <SanityContent value={page.content} />
-      ) : null}
-    </Layout>
+    <article className="post">
+      <div className="featured-image"></div>
+      <header className="entry-header section-inner">
+        {page?.title ? <h2 className="entry-title">{page.title}</h2> : null}
+        {page?.ingress ? <p className="excerpt">{page.ingress} </p> : null}
+      </header>
+      <div className="entry-content section-inner">
+        {page?.content && page.content?.length > 0 ? (
+          <SanityContent value={page.content} />
+        ) : null}
+      </div>
+    </article>
   );
 }
--- app/routes/posts/index.tsx
+++ app/routes/posts/index.tsx
@@ -1,43 +1,70 @@
 import type { LoaderFunction } from "@remix-run/node";
 import { Link, useLoaderData } from "@remix-run/react";
-import groq from "groq";
 
-import Layout from "~/components/Layout";
+import { parseJSON } from "date-fns";
+import { sortBy } from "sort-by-typescript";
+
+import groq from "groq";
 import { client } from "~/sanity/client";
+
+import ContentList from "~/components/ContentList";
+import ContentListItem from "~/components/ContentListItem";
+import type { Post } from "~/models/post.server";
 import type { Page } from "~/sanity/types/Page";
 
 export const loader: LoaderFunction = async () => {
-  const pages = await client.fetch(
-    groq`*[_type == "page"]{ _id, title, slug }`
+  const pageList = await client.fetch(
+    groq`*[_type == "page"]{ _id, title, slug, _updatedAt }`
   );
 
-  return { pages };
+  const pages = pageList.map((page) => ({
+    title: page.title,
+    slug: page.slug.current,
+    date: parseJSON(page._updatedAt),
+  }));
+
+  return groupByYear(pages.sort(sortBy("-date")));
 };
 
 export default function Index() {
-  const { pages } = useLoaderData<{ pages: Page[] }>();
+  const data = useLoaderData<typeof loader>();
+  console.log(data);
+  const entries = data; // as Post[];
+  const years = Object.keys(entries) as string[];
+  const sortedYears = years.sort((a, b) => b.localeCompare(a));
 
-  return (
-    <Layout>
-      {pages.length > 0 ? (
-        <ul className="grid grid-cols-1 gap-6">
-          {pages.map((page) => (
-            <li key={page._id} className="rounded bg-white p-6 shadow md:p-12">
-              {page?.slug?.current ? (
-                <Link
-                  prefetch="render"
-                  to={page?.slug?.current}
-                  className="text-xl font-bold text-green-600 underline hover:text-green-400"
-                >
-                  {page.title}
-                </Link>
-              ) : (
-                <span className="text-xl font-bold">{page.title}</span>
-              )}
-            </li>
-          ))}
-        </ul>
-      ) : null}
-    </Layout>
-  );
+  const postsByYear = sortedYears.map((year) => {
+    const y = parseInt(year, 10);
+    const posts = entries[y] as unknown as any[];
+    const linksByYear = posts.map((post) => (
+      <ContentListItem
+        key={post.slug}
+        post={post}
+        date={post.date}
+        prefetch="intent"
+      />
+    ));
+    return (
+      <li key={year}>
+        <h3 className="list-title">
+          <Link to="#" className="">
+            {year}
+          </Link>
+        </h3>
+        <ul>{linksByYear}</ul>
+      </li>
+    );
+  }) as unknown as string;
+
+  return <ContentList title="Posts" description="" list={postsByYear} />;
+}
+
+function groupByYear(objectArray: Post) {
+  const InitialValue: Post[] = {} as Post[];
+  return objectArray.reduce((acc, obj) => {
+    const key = parseJSON(obj["date"]).getFullYear();
+    (acc[key] as unknown as any[]) ??= [];
+    (acc[key] as unknown as any[]).push(obj);
+    return acc;
+  }, InitialValue);
 }
