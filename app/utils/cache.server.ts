e134bb302d9c91d380aaa87482f9cd6bca738063
Per Søderlind <per@soderlind.no>
2022-09-27 13:16:40 +0200

    Add seconds constants, MINUTE_IN_SECONDS etc

--- app/utils/cache.server.ts
+++ app/utils/cache.server.ts
@@ -1,23 +1,25 @@
 /*
  * @see https://www.npmjs.com/package/node-cache
  */
+
+export const MINUTE_IN_SECONDS = 60;
+export const HOUR_IN_SECONDS = 60 * MINUTE_IN_SECONDS;
+export const DAY_IN_SECONDS = 24 * HOUR_IN_SECONDS;
+export const WEEK_IN_SECONDS = 7 * DAY_IN_SECONDS;
+export const MONTH_IN_SECONDS = 30 * DAY_IN_SECONDS;
+export const YEAR_IN_SECONDS = 365 * DAY_IN_SECONDS;
+
 import NodeCache from "node-cache";
 let cache: NodeCache;
 
 declare global {
   var __cache: NodeCache | undefined;
 }
-// const cacheSettings = {
-//   stdTTL: 60 * 60 * 24,
-//   checkperiod: 60 * 60 * 24,
-// };
 
 if (process.env.NODE_ENV === "production") {
-  // cache = new NodeCache(cacheSettings);
   cache = new NodeCache();
 } else {
   if (!global.__cache) {
-    // global.__cache = new NodeCache(cacheSettings);
     global.__cache = new NodeCache();
   }
   cache = global.__cache;
