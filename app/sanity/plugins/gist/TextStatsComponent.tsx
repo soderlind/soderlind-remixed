d6ca3d08b275350d3e55090d897b03ddd653df33
Per Søderlind <per@soderlind.no>
2022-10-16 22:05:26 +0200

    Add sanity plugins

---
+++ app/sanity/plugins/gist/TextStatsComponent.tsx
@@ -0,0 +1,58 @@
+import type { InputProps, RenderInputCallback } from "sanity";
+import { Stack, Text } from "@sanity/ui";
+
+const isStringOrText = (schemaType) => ["string", "text"].includes(schemaType.name);
+
+const isPortableText = (schemaType) =>
+  schemaType.name === "array" && schemaType.of?.some((type) => type.name === "block");
+
+function toPlainText(blocks) {
+  return (
+    blocks
+      // loop through each block
+      .map((block) => {
+        // if it's not a text block with children,
+        // return nothing
+        if (block._type !== "block" || !block.children) {
+          return "";
+        }
+        // loop through the children spans, and join the
+        // text strings
+        return block.children.map((child) => child.text).join("");
+      })
+      // join the paragraphs leaving split by two linebreaks
+      .join("\n\n")
+  );
+}
+
+interface TextStatsProps {
+  children?: React.ReactNode;
+  value?: string;
+}
+
+export function TextStats({ children, value }: TextStatsProps) {
+  const length = (value && value.length) || 0;
+  return (
+    <Stack space={2}>
+      {children}
+      <Text size={1} muted>
+        {length} characters
+      </Text>
+    </Stack>
+  );
+}
+
+export function renderTextStats(props: InputProps, next: RenderInputCallback) {
+  // console.log(props.value, props.schemaType);
+  if (
+    (isStringOrText(props.schemaType) || isPortableText(props.schemaType)) &&
+    props.schemaType.options?.charCount
+  ) {
+    const textValue = isPortableText(props.schemaType)
+      ? toPlainText(props.value || [])
+      : (props.value as string);
+    return <TextStats value={textValue}>{next(props)}</TextStats>;
+  }
+
+  return next(props);
+}
