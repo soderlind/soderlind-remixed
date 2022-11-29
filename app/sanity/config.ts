5bc5a3d2eccfd73f07ebdeba901ec63b16a273cb
Per Søderlind <per@soderlind.no>
2022-11-29 22:18:28 +0100

    Get env refactored

--- app/sanity/config.ts
+++ app/sanity/config.ts
@@ -6,10 +6,17 @@ import schema from "./schema";
 
 export const projectDetails = () => ({
   projectId:
-    typeof document === "undefined" ? process.env.SANITY_PROJECT_ID : window?.ENV?.projectId,
-  dataset: typeof document === "undefined" ? process.env.SANITY_DATASET : window?.ENV?.dataset,
+    typeof document === "undefined"
+      ? process.env.SANITY_PROJECT_ID
+      : window?.ENV?.projectId, // ?? import.meta.env.SANITY_PROJECT_ID,
+  dataset:
+    typeof document === "undefined"
+      ? process.env.SANITY_DATASET
+      : window?.ENV?.dataset, // ?? import.meta.env.SANITY_DATASET,
   apiVersion:
-    typeof document === "undefined" ? process.env.SANITY_API_VERSION : window?.ENV?.apiVersion,
+    typeof document === "undefined"
+      ? process.env.SANITY_API_VERSION
+      : window?.ENV?.apiVersion, // ?? import.meta.env.SANITY_API_VERSION,
 });
 
 export const config = createConfig({
