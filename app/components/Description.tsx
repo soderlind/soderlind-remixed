5eaa50a79d1c6201976c024b8852d8fb069908cc
Per Søderlind <per@soderlind.no>
2022-11-29 21:58:37 +0100

    Add decription

---
+++ app/components/Description.tsx
@@ -0,0 +1,18 @@
+export default function Description() {
+  return (
+    <>
+      <span>I code</span>
+      <span>
+        <span aria-hidden="true">
+          <span>f</span>
+          <span>
+            o<span></span>
+          </span>
+          <span>r</span>
+        </span>
+        <span className="sr-only">for</span>
+      </span>
+      <span>fun</span>
+    </>
+  );
+}
