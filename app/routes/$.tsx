45f79e1709a468a89d0a6025fb8f58bdca858eaf
Per Søderlind <per@soderlind.no>
2022-11-29 22:17:48 +0100

    Use FindFirstFile to check if file exist

--- app/routes/$.tsx
+++ app/routes/$.tsx
@@ -1,30 +1,30 @@
-import type { LoaderFunction } from "@remix-run/node"; // or cloudflare/deno
+import type { LoaderArgs } from "@remix-run/node"; // or cloudflare/deno
 import { redirect } from "@remix-run/node"; // or cloudflare/deno
 import { useCatch } from "@remix-run/react";
-import { findByName } from "~/utils/fs.server";
+
+import { z } from "zod";
+import { zx } from "zodix";
+import { FindFirstFile } from "~/utils/fs.server";
 
 type Path = {
   path: string;
 };
 type Paths = Path[];
-
 const ignorePaths = ["/about", "/archive", "/post"];
 
-export const loader: LoaderFunction = async ({ request }) => {
-  const url = new URL(request.url);
-  const path = url.pathname;
-  const p = require("path");
-  const archivePath = p.resolve("public/jekyll");
+export const loader = async ({ request }: LoaderArgs) => {
+  const pathname = new URL(request.url).pathname;
+  const path = require("path");
+  const archivePath = path.resolve("app/jekyll");
 
-  const basename = p.basename(path);
-  const dirname = p.dirname(path);
+  const basename = path.basename(pathname);
+  const dirname = path.dirname(pathname);
 
   const ignore = ignorePaths.some((oath) => dirname.startsWith(oath));
   if (!ignore) {
-    const files = await findByName(archivePath, basename);
-
-    if (files && files.length > 0) {
-      const slug = files[0].replace(/\.[^/.]+$/, ""); // remove extension.
+    const file = await FindFirstFile(archivePath, basename);
+    if (file) {
+      const slug = file.replace(/\.[^/.]+$/, ""); // remove extension.
       return redirect(`/archive/${slug}`, 301);
     }
   }
@@ -35,13 +35,15 @@ export const loader: LoaderFunction = async ({ request }) => {
 };
 
 export function CatchBoundary() {
-  let caught = useCatch();
+  const caught = useCatch();
   return (
     <article className="post">
       <div className="featured-image"></div>
       <header className="entry-header section-inner">
         <h1 className="entry-title">You&#x27;re alone here</h1>
-        <p className="excerpt">Oops! Looks like you tried to visit a page that does not exist.</p>
+        <p className="excerpt">
+          Oops! Looks like you tried to visit a page that does not exist.
+        </p>
       </header>
       <div className="entry-content section-inner">{caught.statusText}</div>
     </article>
@@ -50,4 +52,7 @@ export function CatchBoundary() {
 
 export default function Page() {
   return <CatchBoundary />;
+}
+function findFirstFile(archivePath: any, basename: any) {
+  throw new Error("Function not implemented.");
 }
